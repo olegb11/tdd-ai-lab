c88665f0d56d61e7f640c153b5373a8c1a44c2d8
olegb11 <69043745+olegb11@users.noreply.github.com>
2026-09-01 22:13:30 +0300

    Add TODO for total price calculation in Calculator
    Added a TODO comment for implementing total price calculation logic.

--- src/Domain/Calculator.cs
+++ src/Domain/Calculator.cs
@@ -4,6 +4,7 @@ public class Calculator
 {
     public decimal GetTotalByProducts(IEnumerable<Item> items)
     {
+        // TODO: Implement total price calculation logic including percentage discounts
         throw new NotImplementedException();
     }
 }

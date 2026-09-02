f7e48675bb24882902ead9fed10ecfa275926829
Oleg Burlaka <oleg.burlaka@worldapp.com>
2026-09-02 05:50:00 +0300

    chore(tdd): green state [FAST mode] - 02.09.2026  5:50:00,07

---
+++ src/Domain.Tests/AdditionBfTests.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+using Xunit;
+
+namespace Domain.Tests;
+
+public class AdditionBfTests
+{
+    [Theory]
+    [InlineData(2, 3, 5)]
+    [InlineData(0, 0, 0)]
+    [InlineData(10, 15, 25)]
+    [InlineData(100, 50, 150)]
+    public void Addition_TwoBytes_ReturnsCorrectSum(byte a, byte b, byte expected)
+    {
+        // Arrange
+        byte[] input = new byte[] { a, b };
+
+        // Act & Assert
+        byte[] output = BfInterpreter.Execute(BfPrograms.Addition, input);
+
+        Assert.Single(output);
+        Assert.Equal(expected, output[0]);
+    }
+}
---
+++ src/Domain/BfPrograms.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain;
+
+public static class BfPrograms
+{
+    public const string Addition = ",>,[<+>-]<.";
+}
